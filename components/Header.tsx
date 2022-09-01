083f8380b08c678b6fc49a10f345dfe2077e2221
greatAdams01 <kingifean@gmail.com>
2022-09-01 19:18:31 +0100

    Added connect wallet

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,45 +1,58 @@
 import type { NextPage } from 'next'
 import Link from 'next/link';
-import { ConnectButton } from 'web3uikit'
 import Web3Model from 'web3modal'
 import { ethers } from 'ethers';
 import { useState } from 'react';
-const providerOptions = {
+import { shortenAddress } from '../utils/shortAddress';
+import WalletConnectProvider from "@walletconnect/web3-provider";
 
+const providerOptions = {
+  walletconnect: {
+    package: WalletConnectProvider, // required
+    options: {
+      infuraId: process.env.NEXT_PUBLIC_infuraId // required
+    }
+  }
 }
 
 const Header: NextPage = () =>  {
   // eslint-disable-next-line react-hooks/rules-of-hooks
-  const [web3Provider, setWeb3Provider] = useState({})
+  const [web3Provider, setWeb3Provider] = useState<any>({})
+  const [currentAccount, setCurrentAccount] = useState<string | undefined>()
 
-  // const connectWallet = async () => {
+  const connectWallet = async () => {
 
-  //   try {
-  //     let web3modal = new Web3Model({
-  //       cacheProvider: false,
-  //       providerOptions
-  //     })
-  //     const web3ModelInstance = await web3modal.connect()
-  //     const web3modalProvider = new ethers.providers.Web3Provider(web3ModelInstance)
-  //     console.log(web3modalProvider)
-  //     if (web3modalProvider) {
-  //       // eslint-disable-next-line react-hooks/rules-of-hooks
-  //       setWeb3Provider(web3modalProvider)
-  //     }
-  //   } catch (error) {
-  //     console.log(error)
-  //   }
+    try {
+      let web3modal = new Web3Model({
+        cacheProvider: false,
+        providerOptions
+      })
+      const web3ModelInstance = await web3modal.connect()
+      const web3modalProvider = new ethers.providers.Web3Provider(web3ModelInstance)
+      web3modalProvider.send("eth_requestAccounts", [])
+      .then((accounts)=>{
+        if(accounts.length>0) setCurrentAccount(accounts[0])
+      }).catch((e)=>console.log(e))
 
-  // }
+    } catch (error) {
+      console.log(error)
+    }
+
+  }
+
+  const onClickDisconnect = () => {
+    console.log("onClickDisConnect")
+    setCurrentAccount(undefined)
+  }
 
   return(
     <div>
 
-      <nav className='px-5 py-4 flex justify-between'>
+      <nav className='px-5 py-5 flex justify-between'>
         <div className='flex space-x-96'>
-          <img className='w-[125px] h-[33px]' src="/img/usher-logo.png" alt="" />
+          <img className='w-[125px] h-[35px]' src="/img/usher-logo.png" alt="" />
           <div>
-            <ul className='flex space-x-10 pt-1'>
+            <ul className='flex space-x-10 pt-2'>
               <li>
                 <Link href='/'>Home</Link>
               </li>
@@ -50,7 +63,12 @@ const Header: NextPage = () =>  {
           </div>
         </div>
         <div>
-        <ConnectButton moralisAuth={false} />
+          {!currentAccount ? (
+            
+            <button className='p-2 border rounded-xl cursor-pointer' onClick={() => connectWallet()} >Connect wallet</button>
+          ) : <p className='p-2 border rounded-xl cursor-pointer' onClick={() => onClickDisconnect()}>{shortenAddress(currentAccount)}</p>
+
+          }
         </div>
       </nav>
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,62 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import Safe, { SafeFactory, SafeAccountConfig } from '@gnosis.pm/safe-core-sdk'
+import { ethers } from 'ethers'
+import EthersAdapter from '@gnosis.pm/safe-ethers-lib'
+import SafeServiceClient from '@gnosis.pm/safe-service-client'
 import styles from '../styles/Home.module.css'
+import { useMoralis, useChain } from 'react-moralis'
+import Web3Modle from 'web3modal'
+import { useEffect, useState } from 'react'
+
+
 
 const Home: NextPage = () => {
+  const [adapter, setAdapter] = useState<any>()
+  const [address, setAddress] = useState<string[]>()
+
+  const { enableWeb3  } = useMoralis()
+
+// const web3Provider = provider
+
+// const load = async () => {
+//   const web3modal = new Web3Modle()
+//   const connection = await web3modal.connect()
+//   const provider = new ethers.providers.Web3Provider(connection)
+
+//   const safeOwner = provider.getSigner(0)
+
+//   const ethAdapter = new EthersAdapter({
+//     ethers,
+//     signer: safeOwner
+//   })
+
+//   console.log(ethAdapter, safeOwner)
+//   setAdapter(ethAdapter)
+// }
+
+//   const createSafe = async () => {
+
+//   const ethAdapter:EthersAdapter = adapter;
+
+//   const safeFactory = await SafeFactory.create({ ethAdapter })
+
+//   const owners = ['0x<address>', '0x<address>', '0x<address>']
+//   const threshold = 3
+//   const safeAccountConfig: SafeAccountConfig = {
+//     owners,
+//     threshold,
+//     // ...
+//   }
+
+//   const safeSdk: Safe = await safeFactory.deploySafe({ safeAccountConfig }) 
+//   }
+
+
+//   useEffect(() => {
+//     load()
+//   }, [])
+
   return (
     <div className={styles.container}>
       <Head>
